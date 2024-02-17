c218c080c782e8bf83a4eff07edb5c53dd1eed0c
Ahmet Önder ÖNLÜ <115372073+ahmetondermu@users.noreply.github.com>
2024-02-17 10:30:06 +0300

    Add files via upload

---
+++ SQLQUERY3.sql
@@ -0,0 +1,12 @@
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+SELECT * FROM country
+WHERE (country LIKE '%n') AND country LIKE '%______%';
+
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%';
+
+SELECT * FROM film
+WHERE (title LIKE 'C%') AND (length > 90) AND (rental_rate = 2.99);
+
